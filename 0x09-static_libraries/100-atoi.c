aba52fd0af1c6f7b3cc31627a2b678a8585bb582
seankihiu <seankihiu45@gmail.com>
2023-10-28 05:59:24 -0700

    files

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -7,12 +7,12 @@
 */
 int _atoi(char *s)
 {
-int sign = 1, i = 0;
+int sig = 1, i = 0;
 unsigned int res = 0;
 while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 {
 if (s[i] == '-')
-sign *= -1;
+sig *= -1;
 i++;
 }
 while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
@@ -20,6 +20,6 @@ while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 res = (res * 10) + (s[i] - '0');
 i++;
 }
-res *= sign;
+res *= sig;
 return (res);
 }
