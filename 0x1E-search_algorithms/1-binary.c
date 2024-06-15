c4b70ad0843cb702bb50999ec6c9a9284685f98f
Seankihiu <seankihiu45@gmail.com>
2024-06-15 04:30:25 -0700

    files

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,47 +1,61 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Search for a value in a sorted
- * integer array using binary search
- *
- * @array: Pointer to the first element of the array
- * @size: Number of array elements
- * @value: Value to search for
- *
- * Return: The first index where value is located
- * or -1 if value is not found or if array is NULL
- */
+ *  * recursive_search - searches for a value in an array of
+ *   * integers using the Binary search algorithm
+ *    *
+ *     *
+ *      * @array: input array
+ *       * @size: size of the array
+ *        * @value: value to search in
+ *         * Return: index of the number
+ *          */
+int recursive_search(int *array, size_t size, int value)
+{
+		size_t half = size / 2;
+			size_t i;
+
+				if (array == NULL || size == 0)
+							return (-1);
+
+					printf("Searching in array");
+
+						for (i = 0; i < size; i++)
+									printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+							printf("\n");
+
+								if (half && size % 2 == 0)
+											half--;
+
+									if (value == array[half])
+												return ((int)half);
+
+										if (value < array[half])
+													return (recursive_search(array, half, value));
+
+											half++;
+
+												return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ *  * binary_search - calls to binary_search to return
+ *   * the index of the number
+ *    *
+ *     * @array: input array
+ *      * @size: size of the array
+ *       * @value: value to search in
+ *        * Return: index of the number
+ *         */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = size - 1;
-	size_t middle = 0;
-	size_t i = 0;
+		int index;
 
-	if (array != NULL)
-	{
-		while (left <= right)
-		{
-			middle = (left + right) / 2;
-			printf("Searching in array: ");
-			for (i = left; i < right; i++)
-			{
-				printf("%d, ", array[i]);
-			}
-			printf("%d\n", array[i]);
-			if (array[middle] < value)
-			{
-				left = middle + 1;
-			}
-			else if (array[middle] > value)
-			{
-				right = middle - 1;
-			}
-			else
-			{
-				return (middle);
-			}
-		}
-	}
-	return (-1);
+			index = recursive_search(array, size, value);
+
+				if (index >= 0 && array[index] != value)
+							return (-1);
+
+					return (index);
 }
